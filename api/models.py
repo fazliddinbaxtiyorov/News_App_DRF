f21fc44bd95ec92b51a4744c163cbcf70ac27d99
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-11-28 12:32:01 +0500

    Adding: Models information

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class NewsModels(models.Model):
+    title = models.CharField(max_length=80)
+    link = models.URLField(max_length=120)
+    photo = models.ImageField()
+    content = models.CharField(max_length=120)
+    date_time = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'news.db'
